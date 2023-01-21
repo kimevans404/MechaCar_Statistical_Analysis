19a7f12c5f53d7239cb3614bb2bc183efdd79c12
Kimberly <111471057+kimevans404@users.noreply.github.com>
2023-01-21 12:55:23 -0500

    final commit

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,12 +2,12 @@
 library(dplyr)
 
 ### ?read.csv()
-### getwd()
+### getwd
 ### setwd("/Users/kimbers/Desktop/Class_Folder/Unit-5/16-R/MechaCar_Statistical_Analysis")
 
 ### import CSV as dataframe
 mecha_mpg <- read.csv(file="./MechaCar_mpg.csv",check.names=F,stringsAsFactors = F)
-### Perform linear regression, pass in all six variables (i.e., columns), and add prev dataframe as the data parameter.
+### Perform linear regression, pass in all six variables (i.e., columns), and add the prev dataframe as the data parameter.
 mecha_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg)
 ### determine the p-value and the r-squared value for the linear regression model
 summary(mecha_lm)
@@ -16,16 +16,16 @@ summary(mecha_lm)
 ### read csv
 susp_coil <- read.csv(file="./Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
 head(susp_coil)
-### create dataframe for summary:
+### create dataframe to get mean, median, variance, and stand. dev. of the suspension coil’s PSI column
 total_summary <- susp_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 head(total_summary)
-### create lot summaries:
+### generate individual lot summaries
 lot_summary <- susp_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 head(lot_summary)
 
-### t.test for suspension coils:
+### t-test for suspension coils in all lots
 t.test(susp_coil$PSI, mu = 1500)
-#individual lot test:
+### individual lot t-test:
 t.test(subset(susp_coil, Manufacturing_Lot="Lot1")$PSI, mu=1500)
 t.test(subset(susp_coil, Manufacturing_Lot="Lot2")$PSI, mu=1500)
 t.test(subset(susp_coil, Manufacturing_Lot="Lot3")$PSI, mu=1500)
